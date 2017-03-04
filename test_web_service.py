06e89459c8820e7ec51d00171f05dbb2b7913a1e
Dan Oneata <dan.oneata@gmail.com>
2017-03-04 13:32:45 +0200

    Add web-service for prediction

--- test_web_service.py
+++ test_web_service.py
@@ -6,8 +6,38 @@ import requests
 URL = 'http://localhost:6667/predict'
 
 ids = [
-    [],
-    [],
+    {
+        "ACUM": 17,
+        "CHASSIS": 21,
+        "CPU": 320,
+        "DISPLAY": 63,
+        "GPU": 388,
+        "HDD": 8,
+        "MODEL": 1,
+        "MDB": 18,
+        "MEM": 24,
+        "ODD": 0,
+        "SIST": 1,
+        "SHDD": 0,
+        "WAR": 1,
+        "WNET": 35,
+    },
+    {
+        "ACUM": 17,
+        "CHASSIS": 21,
+        "CPU": 320,
+        "DISPLAY": 63,
+        "GPU": 388,
+        "HDD": 8,
+        "MODEL": 1,
+        "MDB": 18,
+        "MEM": 24,
+        "ODD": 0,
+        "SIST": 1,
+        "SHDD": 0,
+        "WAR": 1,
+        "WNET": 35,
+    },
 ]
 
 response = requests.post(URL, data=json.dumps(dict(ids=ids)))
