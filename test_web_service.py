4c638bc33ed66a696414543e65da73d2a623561a
Dan Oneata <dan.oneata@gmail.com>
2017-03-06 16:45:01 +0200

    MINOR Update testing script

--- test_web_service.py
+++ test_web_service.py
@@ -21,24 +21,9 @@ ids = [
         "SHDD": 0,
         "WAR": 1,
         "WNET": 35,
-    },
-    {
-        "ACUM": 17,
-        "CHASSIS": 21,
-        "CPU": 320,
-        "DISPLAY": 63,
-        "GPU": 388,
-        "HDD": 8,
-        "MODEL": 1,
-        "MDB": 18,
-        "MEM": 24,
-        "ODD": 0,
-        "SIST": 1,
-        "SHDD": 0,
-        "WAR": 1,
-        "WNET": 35,
-    },
-]
+    }
+] * 10
 
 response = requests.post(URL, data=json.dumps(dict(ids=ids)))
+print(response.status_code)
 print(json.dumps(json.loads(response.text), indent=4))
